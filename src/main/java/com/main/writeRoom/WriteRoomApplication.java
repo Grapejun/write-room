01ad65a4b92002c3b54eb5ec070d65565dfcf95c
jhsong76 <20190976@sungshin.ac.kr>
2024-01-02 02:50:28 +0900

    [INIT] 프로젝트 초기세팅

---
+++ src/main/java/com/main/writeRoom/WriteRoomApplication.java
@@ -0,0 +1,13 @@
+package com.main.writeRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/writeRoom/WriteRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.writeRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
