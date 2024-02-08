c407c10d5187a2b9ab696252f1d683e93a152d87
jhsong76 <20190976@sungshin.ac.kr>
2024-02-09 02:20:33 +0900

    fix: 중복 태그 제거 필터링 추가 #101

--- src/main/java/com/main/writeRoom/converter/TagConverter.java
+++ src/main/java/com/main/writeRoom/converter/TagConverter.java
@@ -4,7 +4,9 @@ import com.main.writeRoom.domain.Tag;
 
 import com.main.writeRoom.domain.mapping.NoteTag;
 import com.main.writeRoom.web.dto.tag.TagResponseDTO;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 
@@ -21,8 +23,11 @@ public class TagConverter {
     // 노트태그 리스트를 태그 리스트로 바꿔야 함.
 
     public static TagResponseDTO.tagListForRoom toTagListForRoom(Page<NoteTag> noteTags, Long roomId) {
+        Set<Long> uniqueTagIds = new HashSet<>();
         List<TagResponseDTO.tagListForRoomList> toTagListForRoomList = noteTags.stream()
-                .map(tag -> tagListForRoomInfoDTO(tag.getTag())).collect(Collectors.toList());
+                .filter(tag -> uniqueTagIds.add(tag.getTag().getId()))
+                .map(tag -> tagListForRoomInfoDTO(tag.getTag()))
+                .collect(Collectors.toList());
 
         return TagResponseDTO.tagListForRoom.builder()
                 .roomId(roomId)
