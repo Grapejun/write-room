bc6f0acae96edadff4e49a80c35dd0bf788c1eaf
jhsong76 <20190976@sungshin.ac.kr>
2024-02-12 23:25:15 +0900

    feat: 북마크한 노트 리스트 조회 #123

--- src/main/java/com/main/writeRoom/repository/BookmarkNoteRepository.java
+++ src/main/java/com/main/writeRoom/repository/BookmarkNoteRepository.java
@@ -1,7 +1,11 @@
 package com.main.writeRoom.repository;
 
 import com.main.writeRoom.domain.Bookmark.BookmarkNote;
+import com.main.writeRoom.domain.User.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkNoteRepository extends JpaRepository<BookmarkNote, Long> {
+    Page<BookmarkNote> findAllByUser(User user, PageRequest pageRequest);
 }
--- src/main/java/com/main/writeRoom/service/BookmarkService/BookmarkService.java
+++ src/main/java/com/main/writeRoom/service/BookmarkService/BookmarkService.java
@@ -1,10 +1,13 @@
 package com.main.writeRoom.service.BookmarkService;
 
 import com.main.writeRoom.domain.Bookmark.BookmarkMaterial;
+import com.main.writeRoom.domain.Bookmark.BookmarkNote;
 import com.main.writeRoom.domain.User.User;
 import com.main.writeRoom.web.dto.bookmark.BookmarkResponseDTO;
+import org.springframework.data.domain.Page;
 
 public interface BookmarkService {
     BookmarkMaterial postTopic(User user, String content);
     BookmarkResponseDTO.TopicResultDTO deleteMaterial(Long userId, BookmarkMaterial bookmarkMaterial);
+    Page<BookmarkNote> findNoteBookmark(User user, Integer page);
     }
