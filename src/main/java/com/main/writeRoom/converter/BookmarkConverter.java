f88be26e1905f7747eb6356ad0628d6cc5fe1982
jhsong76 <20190976@sungshin.ac.kr>
2024-02-13 12:35:42 +0900

    refactor: 북마크 노트 조회시 응답 데이터 추가 #123

--- src/main/java/com/main/writeRoom/converter/BookmarkConverter.java
+++ src/main/java/com/main/writeRoom/converter/BookmarkConverter.java
@@ -1,9 +1,12 @@
 package com.main.writeRoom.converter;
 
+import static java.util.stream.Collectors.toList;
+
 import com.main.writeRoom.domain.Bookmark.BookmarkMaterial;
 import com.main.writeRoom.domain.Bookmark.BookmarkNote;
+import com.main.writeRoom.domain.mapping.NoteTag;
 import com.main.writeRoom.web.dto.bookmark.BookmarkResponseDTO;
-import com.main.writeRoom.web.dto.note.NoteResponseDTO;
+import com.main.writeRoom.web.dto.tag.TagResponseDTO.TagList;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
@@ -35,7 +38,7 @@ public class BookmarkConverter {
                         .id(bookmarkMaterial.getId())
                         .content(bookmarkMaterial.getContent())
                         .build())
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return BookmarkResponseDTO.BookMarkMaterialListDTO.builder()
                 .isLast(bookmarkMaterialPage.isLast())
@@ -48,17 +51,29 @@ public class BookmarkConverter {
     }
 
     public static BookmarkResponseDTO.NoteListForNoteBookmark toNoteBookmarkListResult(Page<BookmarkNote> bookmarkNotePage) {
-        List<BookmarkResponseDTO.NoteListForNoteBookmarkList> bookmarkLists = bookmarkNotePage
+        List<BookmarkResponseDTO.NoteListForNoteBookmarkList> bookmarkLists = bookmarkNotePage.getContent()
                 .stream()
-                .map(bookmarkNote -> BookmarkResponseDTO.NoteListForNoteBookmarkList.builder()
-                        .noteId(bookmarkNote.getNote().getId())
-                        .noteTitle(bookmarkNote.getNote().getTitle())
-                        .noteContent(bookmarkNote.getNote().getContent())
-                        .writer(bookmarkNote.getNote().getUser().getName())
-                        .writerImg(bookmarkNote.getNote().getUser().getProfileImage())
-                        .createdAt(bookmarkNote.getNote().getCreatedAt())
-                        .build())
-                .toList();
+                .map(bookmarkNote -> {
+                    List<TagList> tagList = bookmarkNote.getNote().getNoteTagList()
+                            .stream()
+                            .map(NoteTag::getTag)
+                            .map(tag -> new TagList(tag.getId(), tag.getContent()))
+                            .toList();
+
+                    return BookmarkResponseDTO.NoteListForNoteBookmarkList.builder()
+                            .roomId(bookmarkNote.getRoom().getId())
+                            .noteId(bookmarkNote.getNote().getId())
+                            .noteTitle(bookmarkNote.getNote().getTitle())
+                            .noteSubtitle(bookmarkNote.getNote().getSubtitle())
+                            .noteContent(bookmarkNote.getNote().getContent())
+                            .noteImg(bookmarkNote.getNote().getCoverImg())
+                            .writer(bookmarkNote.getNote().getUser().getName())
+                            .writerImg(bookmarkNote.getNote().getUser().getProfileImage())
+                            .createdAt(bookmarkNote.getNote().getCreatedAt())
+                            .tagList(tagList)
+                            .build();
+                })
+                .collect(Collectors.toList());
 
         return BookmarkResponseDTO.NoteListForNoteBookmark.builder()
                 .isLast(bookmarkNotePage.isLast())
