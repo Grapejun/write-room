97ed7f354a452727440da96ac8b194dee55601fe
jhsong76 <20190976@sungshin.ac.kr>
2024-02-13 14:09:07 +0900

    build: 데이터 저장 시간 변경 #12

--- src/main/java/com/main/writeRoom/WriteRoomApplication.java
+++ src/main/java/com/main/writeRoom/WriteRoomApplication.java
@@ -1,5 +1,7 @@
 package com.main.writeRoom;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 public class WriteRoomApplication {
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WriteRoomApplication.class, args);
